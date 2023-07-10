02bdb0239d40449d44a4e8d81462758e85479220
Shalini-190 <skshalu19@gmail.com>
2023-07-10 19:59:25 +0530

    Committing

---
+++ Module_2/HTML/JS/Array1.js
@@ -0,0 +1,10 @@
+let nos=[23,19,1,2,3,4,5,6]
+console.log("The initial array is:",nos);//print the initial array
+console.log("The length of the array is:",nos.length);//print the length of the array
+//print the first element
+console.log("The 1st element of array is:",nos[0]);
+//last element
+console.log("The last element of array is:",nos[nos.length-1]);
+
+
+
